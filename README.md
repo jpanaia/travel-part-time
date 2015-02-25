48e10423e0da0459644f01d141424bd6d475b09b
Jaime <jpanaia@gmail.com>
2015-02-25 11:56:49 -0500

    added seed, updated readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 <h1>Travel Site for Part Timers</h1>
 
-<p><pre>git clone https://github.com/jpanaia/travel-part-time.git</pre></p>
+<p>Site that's ready for Google Maps! Follow command prompts below to copy down this repository and seed the database.</p>
+
+<p><pre>$ git clone https://github.com/jpanaia/travel-part-time.git</pre></p>
+<p><pre>$ rake db:seed</pre></p>
+<p><pre>$ rake db:migrate</pre></p>
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Destination.create!([
+  {city: "Paris", country: "France", description: "ooh lala", population: 9999890, avatar_file_name: "paris.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 154900, avatar_updated_at: "2015-02-25 16:43:27"},
+  {city: "Nice", country: "France", description: "Somewhere in france", population: nil, avatar_file_name: "nice.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 83608, avatar_updated_at: "2015-02-25 16:33:59"},
+  {city: "Aukland ", country: "New Zealand", description: "Where the hobbits live.", population: nil, avatar_file_name: "auckland.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 150256, avatar_updated_at: "2015-02-25 16:34:29"},
+  {city: "Wellington", country: "New Zealand", description: "Another city in hobbit land", population: nil, avatar_file_name: "Wellington.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 128732, avatar_updated_at: "2015-02-25 16:38:31"},
+  {city: "Macchu Picchu", country: "Peru", description: "Home of the Inca Trail", population: nil, avatar_file_name: "Machu-Picchu-Peru-728x490.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 131410, avatar_updated_at: "2015-02-25 16:36:01"},
+  {city: "Sao Paulo", country: "Brazil", description: "Awesome!!", population: 9999999, avatar_file_name: "sao-paulo.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 404019, avatar_updated_at: "2015-02-25 16:40:22"}
+])
