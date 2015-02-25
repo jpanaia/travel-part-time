3ae564441e303c968931327739c57cbf0ac01358
Jaime <jpanaia@gmail.com>
2015-02-25 11:50:23 -0500

    updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
   	@homeland = 'Italy'
   	@countries = ['Norway','Sweden','Peru']
+    @pics = ['peru.jpg','peru2.jpg','peru3.jpg', 'peru4.jpg'].shuffle!
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :destinations
+
   root 'welcome#index'
 
   get 'about' => 'welcome#about'
